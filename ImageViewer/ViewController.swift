34029f8cf909a5f5c7dcd9a58897c84596ed0779
esvxense <esvxense@gmail.com>
2024-03-27 21:33:48 +0300

    Initial Commit

---
+++ ImageViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageViewer
+//
+//  Created by Alexander Snitko on 27.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
